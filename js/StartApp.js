389318850dda1f21fef02bcfb731e1fb5f2fa0e1
ThongZen <5694078+pthong@users.noreply.github.com>
2018-03-21 12:29:02 +0100

    Try to revert

--- js/StartApp.js
+++ js/StartApp.js
@@ -3,14 +3,5 @@ window.onload = function(){
     //defaultTheme = new Theme();
     //originalBleu = new Theme("myTheme","#123456","##EDCBA9");
     themeController = new ThemeController();    
-	countController = new CountController();
-}
-
-function doSomething(){
-	var teller = 9728;
-	setInterval(function(){
-		teller++;
-		document.querySelector("#time").innerHTML = "&#" + teller + " " + "&#"+ (teller+1)+ " " + "&#"+ (teller+2);
-	},500);
 }
 
